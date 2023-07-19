520fdd9f127634ba84fbaaf4eccd444d0e43041c
egai <egaiosowor@yahoo.com>
2023-07-19 13:16:41 -0700

    initial commit

---
+++ superAdmin/includes/footer.php
@@ -0,0 +1,9 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    <div class="col-sm-6">
+                    &copy; <?php echo date('Y')?> Student Grading System - Developed By CSC 481 Group
+                    </div>
+                </div>
+            </div>
+        </footer>
